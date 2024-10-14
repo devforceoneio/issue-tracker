92c224f42dc09601cb54355c30d90e4ae5b1a3d7
Mark Thompson <dev@devforceone.io>
2024-10-15 05:23:45 +1100

    Add Sentry and reinitialize MySQL migration

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://16a25862e090064ccf2af8e28aa33f7a@o4508114341265408.ingest.us.sentry.io/4508114343755776",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
